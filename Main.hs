5d9d37ffe26c3327c992b9fdedf9678dd9dbe5b5
Shoumodip Kar <shoumodipkar@gmail.com>
2021-06-10 13:38:54 +0530

    Code cleanup and use of proper type for file paths

--- Main.hs
+++ Main.hs
@@ -7,18 +7,18 @@ import qualified Data.Text                 as Text
 import qualified Data.Text.IO              as Text
 import qualified Data.Text.Internal.Search as Text
 
-processLine :: String -> (Int, Text.Text) -> Maybe Text.Text
+processLine :: FilePath -> (Int, Text.Text) -> Maybe Text.Text
 processLine fileName (line, contents) = result
   where indices  = Text.indices (Text.pack "TODO: ") contents
         column   = head indices + 6
 
         location = ":" ++ show line ++ ":" ++ show column ++ ": "
         position = Text.pack $ fileName ++ location
-        message  = Text.concat [position, Text.drop column contents]
 
+        message  = Text.concat [position, Text.drop column contents]
         result   = if null indices then Nothing else Just message
 
-findTodos :: String -> Text.Text -> [Text.Text]
+findTodos :: FilePath -> Text.Text -> [Text.Text]
 findTodos fileName = map fromJust
   . filter isJust
   . init
@@ -26,12 +26,12 @@ findTodos fileName = map fromJust
   . zip [1..]
   . Text.lines
 
-searchFile :: String -> IO ()
-searchFile fileName = Text.readFile fileName >>= \lines ->
-    Text.putStrLn
-    $ Text.init
-    $ Text.unlines
-    $ findTodos fileName lines
+searchFile :: FilePath -> IO ()
+searchFile fileName = Text.readFile fileName >>=
+  Text.putStrLn
+  . Text.init
+  . Text.unlines
+  . findTodos fileName
 
-main :: IO [()]
-main = getArgs >>= mapM searchFile
+main :: IO ()
+main = getArgs >>= mapM_ searchFile
