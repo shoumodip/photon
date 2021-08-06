9f1c14b068d253ed5d5d718dfd218b51dd32bb59
Shoumodip Kar <shoumodipkar@gmail.com>
2021-08-06 23:41:14 +0530

    Code cleanup

--- Main.hs
+++ Main.hs
@@ -3,32 +3,28 @@ module Main where
 import Data.Maybe
 import System.Environment
 
-import qualified Data.Text                 as Text
-import qualified Data.Text.IO              as Text
-import qualified Data.Text.Internal.Search as Text
+import qualified Data.Text                 as T
+import qualified Data.Text.IO              as T
+import qualified Data.Text.Internal.Search as T
 
-processLine :: FilePath -> (Int, Text.Text) -> Maybe Text.Text
-processLine fileName (line, contents) = result
-  where indices  = Text.indices (Text.pack "TODO: ") contents
-        column   = head indices + 6
+processLine :: FilePath -> (Int, T.Text) -> Maybe T.Text
+processLine fileName (line, contents)
+  | null indices = Nothing
+  | otherwise    = Just $ T.pack position <> T.drop column contents
+  where
+    indices  = T.indices (T.pack "TODO: ") contents
+    column   = head indices + 6
+    position = concat [fileName, ":", show line, ":", show column, ": "]
 
-        location = ":" ++ show line ++ ":" ++ show column ++ ": "
-        position = Text.pack $ fileName ++ location
-
-        message  = Text.concat [position, Text.drop column contents]
-        result   = if null indices then Nothing else Just message
-
-findTodos :: FilePath -> Text.Text -> [Text.Text]
-findTodos fileName = map fromJust
-  . filter isJust
-  . map (processLine fileName)
+findTodos :: FilePath -> T.Text -> [T.Text]
+findTodos fileName = mapMaybe (processLine fileName)
   . zip [1..]
-  . Text.lines
+  . T.lines
 
 searchFile :: FilePath -> IO ()
-searchFile fileName = Text.readFile fileName >>=
-  Text.putStrLn
-  . Text.unlines
+searchFile fileName = T.readFile fileName >>=
+  T.putStrLn
+  . T.unlines
   . findTodos fileName
 
 main :: IO ()
